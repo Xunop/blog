a2d2e548f1eaf6c351e845c689b98f1e7c2ac938
xun <xun794@gmail.com>
2023-06-06 11:04:44 +0800

    debug

--- source/test/tmp.sh
+++ source/test/tmp.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-date=$(date +%Y-%m-%d)
-echo $date
-date=$(sed -n '1,20{/^date:/{p;q}}' test.md | awk '{ print $2}')
-
-if [ -z "$date" ]; then
-  date=$(date +%Y-%m-%d)
-fi
-echo $date
-
+head_info="sadasda\n111"
+echo -e "$head_info" | sed "1i \\$cat($1)"
